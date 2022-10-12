99036333312341b4ce85d757e147f3bbf44ea8f8
Xydint <78959975+Xydint@users.noreply.github.com>
2022-10-12 20:02:59 +0700

    Initial commit

---
+++ lab2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lab2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RomanNumber n1, n2;
+            RomanNumber[] array = { new RomanNumber(10),
+                new RomanNumber(32323), new RomanNumber(10000),
+                new RomanNumber(4521),new RomanNumber(1),
+                new RomanNumber(11),new RomanNumber(10)};
+
+            n1 = new RomanNumber(23456);
+            n2 = new RomanNumber(5123);
+
+            Console.WriteLine("Unsorted array:");
+            foreach (RomanNumber n in array)
+            {
+                Console.Write("{0} ", n.ToString());
+            }
+            Array.Sort(array);
+            Console.WriteLine("\nSorted array:");
+            foreach (RomanNumber n in array)
+            {
+                Console.Write("{0} ", n.ToString());
+            }
+            Console.WriteLine("");
+
+            // n1: MMMMMMMMMMMMMMMMMMMMMMMCDLVI
+            // n2: MMMMMCXXIII
+            Console.WriteLine("n1: {0}\nn2: {1}", (n1).ToString(), (n2).ToString());
+
+            // n1+n2: MMMMMMMMMMMMMMMMMMMMMMMMMMMMDLXXIX
+            Console.WriteLine("n1+n2: {0}", (RomanNumber.Add(n1, n2)).ToString());
+
+            // n1-n2: MMMMMMMMMMMMMMMMMMCCCXXXIII
+            Console.WriteLine("n1-n2: {0}", (RomanNumber.Sub(n1, n2)).ToString());
+
+            // n1/n2: IV
+            Console.WriteLine("n1/n2: {0}", (RomanNumber.Div(n1, n2)).ToString());
+
+            try
+            {
+                Console.WriteLine("n1*n2: {0}", (RomanNumber.Mul(n1, n2)).ToString());
+            }
+            catch (RomanNumberException e)
+            {
+                Console.WriteLine($"Found exception: {e.Message}");
+            }
+            finally
+            {
+                n1 = new RomanNumber(2509);
+                n2 = new RomanNumber(5);
+                // n1*n2: MMMMMMMMMMMMDXLV
+                Console.WriteLine("2509*5: {0}", (RomanNumber.Mul(n1, n2)).ToString());
+            }
+        }
+    }
+}
---
+++ lab2/RomanNumber.cs
@@ -0,0 +1,79 @@
+﻿namespace lab2
+{
+    internal class RomanNumber : ICloneable, IComparable
+    {
+        // internal конструктор нет возможности вызвать из main
+        public RomanNumber(ushort n)
+        {
+            int i;
+            if (n == 0)
+                throw new RomanNumberException("Constructor: n is equal to zero.");
+            Number = n;
+            RomanString = "";
+            for (i = 0; i < Arr_Size; ++i)
+            {
+                while (n - Values[i] >= 0)
+                {
+                    RomanString += Romans[i];
+                    n -= Values[i];
+                }
+            }
+        }
+        public static RomanNumber Add(RomanNumber? n1, RomanNumber? n2)
+        {
+            if (n1 == null || n2 == null)
+                throw new RomanNumberException("Add: at least one object is null.");
+            if (n1.Number + n2.Number > ushort.MaxValue)
+                throw new RomanNumberException("Add: sum exceeds ushort max value.");
+            return new RomanNumber((ushort)(n1.Number + n2.Number));
+        }
+        public static RomanNumber Sub(RomanNumber? n1, RomanNumber? n2)
+        {
+            if (n1 == null || n2 == null)
+                throw new RomanNumberException("Sub: at least one object is null.");
+            if (n1.Number <= n2.Number)
+                throw new RomanNumberException("Sub: result is less than or equal to zero.");
+            return new RomanNumber((ushort)(n1.Number - n2.Number));
+        }
+        public static RomanNumber Mul(RomanNumber? n1, RomanNumber? n2)
+        {
+            if (n1 == null || n2 == null)
+                throw new RomanNumberException("Mul: at least one object is null.");
+            if (n1.Number * n2.Number > ushort.MaxValue)
+                throw new RomanNumberException("Mul: result exceeds ushort max value.");
+            return new RomanNumber((ushort)(n1.Number * n2.Number));
+        }
+        public static RomanNumber Div(RomanNumber? n1, RomanNumber? n2)
+        {
+            if (n1 == null || n2 == null)
+                throw new RomanNumberException("Div: at least one object is null.");
+            if (n1.Number / n2.Number == 0)
+                throw new RomanNumberException("Div: result equals zero.");
+            return new RomanNumber((ushort)(n1.Number / n2.Number));
+        }
+        public override string ToString()
+        {
+            return RomanString;
+        }
+        // Clone и CompareTo пришлось добавить, иначе класс был бы абстрактным.
+        public object Clone()
+        {
+            return new RomanNumber(Number);
+        }
+        public int CompareTo(object? obj)
+        {
+            if (obj is RomanNumber)
+                return this.Number.CompareTo(((RomanNumber)obj).Number);
+            throw new RomanNumberException("CompareTo: obj is null or not RomanNumber.");
+        }
+        private static int Arr_Size = 13;
+        private static string[] Romans =
+            { "M", "CM", "D", "CD", "C", "XC", "L",
+            "XL", "X", "IX", "V", "IV", "I" };
+        private static ushort[] Values =
+            {1000, 900, 500,
+            400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        private ushort Number;
+        private string RomanString;
+    }
+}
